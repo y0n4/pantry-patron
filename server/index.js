554b84cd01b82f14b82a62b657a098283c67059e
Mealear <mileyy.mk@gmail.com>
2018-05-31 16:51:48 -0700

    Add Editable List Item component and remove View script tag in html

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express');
 const bodyParser = require('body-parser');
 const database = require('../database/index.js');
@@ -34,6 +35,32 @@ app.get('*', (req, res) => {
 });
 
 const port = 3000;
+=======
+let express = require('express');
+let app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hello from the home page!');
+});
+
+app.get('/login', function(req, res) {
+  res.send('Hello world from the PantryPatron login page!')
+});
+
+app.get('/register', function (req, res) {
+  res.send('Hello from the register page!');
+});
+
+app.get('/logout', function(req, res) {
+  res.send('Hello from the logout page!');
+});
+
+app.get('/list', function(req, res) {
+  res.send('Hello from the lists page!');
+});
+
+let port = 3000;
+>>>>>>> Set up endpoints for pages
 
 app.listen(process.env.port || port, function() {
   console.log(`Listening on port ${port}`);
