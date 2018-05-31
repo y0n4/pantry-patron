6ad505b36827735f72f935c53aca40c746a4b3aa
Mealear <mileyy.mk@gmail.com>
2018-05-31 12:03:32 -0700

    Make imports immutable and fix 'send' to 'end'

--- server/index.js
+++ server/index.js
@@ -1,32 +1,33 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const database = './database/index.js';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.get('/home', function(req, res) {
-  res.send('Hello from the home page!');
+  res.end('Hello from the home page!');
 });
 
 app.get('/login', function(req, res) {
-  res.send('Hello world from the PantryPatron login page!')
+  res.end('Hello world from the PantryPatron login page!')
 });
 
 app.get('/register', function (req, res) {
-  res.send('Hello from the register page!');
+  res.end('Hello from the register page!');
 });
 
 app.get('/logout', function(req, res) {
-  res.send('Hello from the logout page!');
+  res.end('Hello from the logout page!');
 });
 
 app.get('/list', function(req, res) {
-  res.send('Hello from the lists page!');
+  res.end('Hello from the lists page!');
 });
 
-let port = 3000;
+const port = 3000;
 
 app.listen(process.env.port || port, function() {
   console.log(`Listening on port ${port}`);
