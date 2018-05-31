7ad2beab438215a16684b661666cac5882de88b3
Mealear <mileyy.mk@gmail.com>
2018-05-31 11:20:06 -0700

    Fix homepage endpoint, include bodyParser and static file dir

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
-app.get('/', function(req, res) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/home', function(req, res) {
   res.send('Hello from the home page!');
 });
 
