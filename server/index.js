0a12932ffb20e35800256576bd13fc3dbda5dd8a
Mealear <mileyy.mk@gmail.com>
2018-05-31 17:04:41 -0700

    Remove merge conflicts

--- server/index.js
+++ server/index.js
@@ -35,32 +35,6 @@ app.get('*', (req, res) => {
 });
 
 const port = 3000;
-=======
-let express = require('express');
-let app = express();
-
-app.get('/', function(req, res) {
-  res.send('Hello from the home page!');
-});
-
-app.get('/login', function(req, res) {
-  res.send('Hello world from the PantryPatron login page!')
-});
-
-app.get('/register', function (req, res) {
-  res.send('Hello from the register page!');
-});
-
-app.get('/logout', function(req, res) {
-  res.send('Hello from the logout page!');
-});
-
-app.get('/list', function(req, res) {
-  res.send('Hello from the lists page!');
-});
-
-let port = 3000;
->>>>>>> Set up endpoints for pages
 
 app.listen(process.env.port || port, function() {
   console.log(`Listening on port ${port}`);
