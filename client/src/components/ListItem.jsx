4a89bbd7f4705d867f126a52926b4e622aaf3f5d
Mealear <mileyy.mk@gmail.com>
2018-06-01 11:24:37 -0700

    Create Item component and a delete and edit button for each item

---
+++ client/src/components/ListItem.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import $ from 'jquery';
+import Client from '../index.jsx';
+
+class ListItem extends React.Component {
+  // delete item
+  handleTrashClick() {
+    this.props.onTrashClick(this.props.id);
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>
+            <b>Item:</b> {this.props.item.name} <br></br>
+            <b>Quantity:</b> {this.props.item.quantity} <br></br>
+            <b>Price:</b> ${Number(this.props.item.price).toFixed(2)}
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <span onClick={this.props.onEditClick}>
+          <a href="#">
+            <span className="glyphicon glyphicon-pencil"></span>
+          </a>
+        </span>
+      </div>
+
+      <div className="col-md-2">
+        <span onClick={this.handleTrashClick.bind(this)}>
+          <a href="#">
+            <span className="glyphicon glyphicon-remove"></span>
+          </a>
+        </span>
+      </div>
+    )
+  }
+}
+
+export default ListItem;
