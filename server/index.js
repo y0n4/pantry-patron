0f83a36b6710070760b61fa5519ad45e52ff77ef
JohnWebb4 <John.Webb94@gmail.com>
2018-06-01 15:42:14 -0700

    Redirect login and register if missing information

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+
 const database = require('../database/index.js');
+
 const app = express();
 const path = require('path');
-// WE NEED BCRYPT HERE TOO
+const bcrypt = require('bcrypt');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+}));
+
+app.use(express.static(`${__dirname}/../client/dist`));
 
 // app.get('/home', function(req, res) {
 //   database.find();
@@ -31,11 +40,39 @@ app.use(express.static(__dirname + '/../client/dist'));
 // });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname+'/../client/dist', 'index.html'));
+  res.sendFile(path.resolve(`${__dirname}/../client/dist`, 'index.html'));
+});
+
+app.post('/login', (req, res) => {
+  // Validate user
+  const user = req.body.user;
+  const password = req.body.password;
+
+  if (user && password) {
+
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.post('/logout', (req, res) => {
+  // Remove user
+});
+
+app.post('/register', (req, res) => {
+  // Create new user
+  const user = req.body.user;
+  const password = req.body.password;
+
+  if (user && password) {
+
+  } else {
+    res.redirect('/register');
+  }
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
