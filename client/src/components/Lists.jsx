56b41541360b16387e4cfdd46e28603c5436f4fb
romcar <rommel.carrasquilla@gmail.com>
2018-06-01 13:47:17 -0400

    create barebones Lists component

---
+++ client/src/components/Lists.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Lists extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+    <div>THIS IS A LIST</div>
+    );
+  }
+}
+
+export default Lists;
