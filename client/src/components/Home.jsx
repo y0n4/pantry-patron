c089adb42fba98b9a165d230a76978991526013f
JohnWebb4 <John.Webb94@gmail.com>
2018-05-31 17:42:55 -0700

    Create home page

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lists: props.lists || [],
+      selected: props.selected,
+      onEdit: props.onEdit,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <div>
+            <p>Chat here</p>
+          </div>
+          <div>
+            <p>Verbal Stats Here</p>
+          </div>
+        </div>
+        <div>
+          <h1>Grocery Lists</h1>
+          <select>
+            {
+              this.state.lists.map((list, index) => {
+                return <option value={list.name} key={index}>{list.name}</option>;
+              })
+            }
+          </select>
+          <div>
+            <p>Grocery List {this.state.selected.name} totals ${this.state.selected.total-price}</p>
+            {
+              this.state.selected.items.map((item, index) => {
+                return <p>{item.name}</p>
+              })
+            }
+            <input type="button" value="Edit" onClick={this.state.onEdit}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
