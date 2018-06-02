61d3c1e6417c2bd3a58c1ffda26a3d3f1056bf00
romcar <rommel.carrasquilla@gmail.com>
2018-06-01 21:21:59 -0400

    might delete this file

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -1,3 +1,4 @@
+/*MAYBE DELETE*/
 import React from 'react';
 import $ from 'jquery';
 import Client from '../index.jsx';
@@ -11,29 +12,31 @@ class ListItem extends React.Component {
   render() {
     return (
       <div>
-        <ul>
-          <li>
-            <b>Item:</b> {this.props.item.name} <br></br>
-            <b>Quantity:</b> {this.props.item.quantity} <br></br>
-            <b>Price:</b> ${Number(this.props.item.price).toFixed(2)}
-          </li>
-        </ul>
-      </div>
+        <div>
+          <ul>
+            <li>
+              <b>Item:</b> {this.props.item.name} <br></br>
+              {/*<b>Quantity:</b> {this.props.item.quantity} <br></br>
+              <b>Price:</b> ${Number(this.props.item.price).toFixed(2)}*/}
+            </li>
+          </ul>
+        </div>
 
-      <div>
-        <span onClick={this.props.onEditClick}>
-          <a href="#">
-            <span className="glyphicon glyphicon-pencil"></span>
-          </a>
-        </span>
-      </div>
+        <div>
+          <span onClick={this.props.onEditClick}>
+            <a href="#">
+              <span className="glyphicon glyphicon-pencil"></span>
+            </a>
+          </span>
+        </div>
 
-      <div className="col-md-2">
-        <span onClick={this.handleTrashClick.bind(this)}>
-          <a href="#">
-            <span className="glyphicon glyphicon-remove"></span>
-          </a>
-        </span>
+        <div className="col-md-2">
+          <span onClick={this.handleTrashClick.bind(this)}>
+            <a href="#">
+              <span className="glyphicon glyphicon-remove"></span>
+            </a>
+          </span>
+        </div>
       </div>
     )
   }
