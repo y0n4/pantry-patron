e6e0791c35e10f8ca7f89ff2b3aa35fb79ed49cb
romcar <rommel.carrasquilla@gmail.com>
2018-06-01 21:23:51 -0400

    refactored - added react-router Links to redirect to pages

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-
+import React from 'react';
+import {Link} from 'react-router-dom';
 const NavBar = ({onHome, onLists, onLogout}) => (
   <div>
     <div>
-      <img src="" alt="Pantry Patron Logo" />
+      <img src="https://i1.wp.com/www.upbring.org/wp-content/uploads/2017/03/fake-logo-2.png?ssl=1" alt="Pantry Patron Logo" />
       <h1>Pantry Patron</h1>
     </div>
     <div>
-      <input type="button" value="Home" onClick={onHome}/>
+      <Link to="/">Home </Link>
+      <Link to="/lists">Lists </Link>
+      <Link to="/logout">Logout </Link>
+      {/*<input type="button" value="Home" onClick={onHome}/>
       <input type="button" value="Lists" onClick={onLists}/>
-      <input type="button" value="Logout" onClick={onLogout}/>
+      <input type="button" value="Logout" onClick={onLogout}/>*/}
     </div>
   </div>
 );
