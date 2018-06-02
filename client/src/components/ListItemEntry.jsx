b169cde9dd45248dbb402a20be192d28932466e3
romcar <rommel.carrasquilla@gmail.com>
2018-06-01 21:22:37 -0400

    added categories to each ListItemEntry

--- client/src/components/ListItemEntry.jsx
+++ client/src/components/ListItemEntry.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Category from './Category.jsx'
 export default class ListItemEntry extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       item: props.item,
-      qty: 0,
+      quantity: 0,
       price: 0.00
 
     };
@@ -19,12 +19,14 @@ export default class ListItemEntry extends React.Component {
   }
 
   render() {
+    var cats = [{name: 'food'}, {name: 'self-care'}];
     return (
       <tr>
         <td>
         <input type="text" name="item" value={this.state.item.name} disabled/>
-        <input type="number" name="qty" value={this.state.qty} onChange={this.handleQtyChange.bind(this)}/>
-        <input type="number" name="price" value={this.state.price} onChange={this.handlePriceChange.bind(this)}/>
+        <input type="number" name="quantity" value={this.state.quantity} onChange={this.handleQtyChange.bind(this)} step="any"/>
+        <input type="number" name="price" value={this.state.price} onChange={this.handlePriceChange.bind(this)} step="any"/>
+        <Category categories={cats}/>
         </td>
       </tr>
     );
