849f03dbbfab78705df3ff8b1dbfb1fe3c66025a
JohnWebb4 <John.Webb94@gmail.com>
2018-06-04 11:52:11 -0700

    Add store schema to database

--- database/index.js
+++ database/index.js
@@ -3,14 +3,14 @@
 const mongoose = require('mongoose');
 
 // establish connection
-const uri = process.env.MONGOOSE_URI || 'mongodb://localhost/pantry-patron'
+const uri = process.env.MONGOOSE_URI || 'mongodb://localhost/pantry-patron';
 mongoose.connect(uri);
 const db = mongoose.connection;
 // feedback from database connection
-db.on('error', function() {
+db.on('error', () => {
   console.log('No connection to database');
 });
-db.once('open',function() {
+db.once('open', () => {
   console.log('Database connection established');
 });
 // import collections here
@@ -18,20 +18,20 @@ const User = require('./schemas/UserSchema.js');
 const GroceryList = require('./schemas/GroceryListSchema.js');
 const Items = require('./schemas/ItemSchema.js');
 const Category = require('./schemas/CategorySchema.js');
+const Store = require('./schemas/StoreSchema.js');
 
-var save = function(user) {
-  var test = new User(user);
+const save = (user) => {
+  let test = new User(user);
 
-  test.save(function(err) {
-    if(err) throw err;
-    console.log(test)
-  })
+  test.save((err) => {
+    if (err) throw err;
+    console.log(test);
+  });
   console.log('inside save function (DB)');
 };
 
-var find = function() {
- console.log('inside find function (DB)');
-
+const find = () => {
+  console.log('inside find function (DB)');
 };
 
 module.exports.save = save;
