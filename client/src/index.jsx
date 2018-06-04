0306ccba1ff65c7cb00837e2897293a9805b10eb
romcar <rommel.carrasquilla@gmail.com>
2018-06-04 16:41:13 -0400

    connected client to POST login request

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -22,7 +22,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      /* Fill me up, buttercup */
+      isLoggedIn :false
     };
   } // end constructor
 
@@ -37,11 +37,20 @@ class App extends React.Component {
         force refresh/ redirect to login
     */
     $.ajax({
-      url: 'http://localhost:3000/login'
+      url: 'http://localhost:3000/login',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(credentials),
+      success: (res) => {
+
+      },
+      error: (err) => {
+        console.error(err);
+      }
     });
   } // end verify
 
-  sendNewUserCredentials(newUserCreds) {
+  sendNewUserCredentials(newUserCreds, callback) {
     /*
       maybe create an endpoint in the server that saves new user info to db
       make a post ajax call to save new user information to database
@@ -61,9 +70,9 @@ class App extends React.Component {
       type: 'POST',
       contentType: 'application/json',
       data: JSON.stringify(newUserCreds),
-      success: (response) => {
-        console.log(response);
+      success: (loc) => {
         console.log('New user information saved to db')
+        callback(loc);
       },
       error: (err) => {
         console.error(err);
@@ -81,12 +90,12 @@ class App extends React.Component {
           <Route exact path="/" render={(props)=>(
             <Home {...props}/>
           )} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" render={() => (<Register grabUserCredentials={grabCredentials}/>)} />
+          <Route exact path="/login" render={(props) => (
+            <Login verify={this.verify.bind(this)} {...props}/>
+          )} />
+          <Route exact path="/register" render={(props) => (<Register grabUserCredentials={grabCredentials} {...props}/>)}/>
           <Route exact path="/logout" component={Login} />
           <Route exact path="/lists" component={Lists} />
-          {/*          <Route exact path='/test' component={}/>
-*/}
         </Switch>
       </Router>
     );
