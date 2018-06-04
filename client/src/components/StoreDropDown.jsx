7fc47bcdb3c829fd5f41301b4cbfcf860eaf5a60
romcar <rommel.carrasquilla@gmail.com>
2018-06-04 18:28:53 -0400

    created a store drop down component

---
+++ client/src/components/StoreDropDown.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const StoreDropDown = (pros) => (
+  constructor(props) {
+    super(props);
+    this.state = {};
+  } // end constructor
+
+  render() {
+    return (
+
+    ); // end return
+  } // end render
+
+);
