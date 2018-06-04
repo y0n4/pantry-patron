eb034299bb597cb4d7cc9a654bd791f0945882b8
romcar <rommel.carrasquilla@gmail.com>
2018-06-04 18:26:49 -0400

    added drop down menu with available stores

--- client/src/components/ListEntry.jsx
+++ client/src/components/ListEntry.jsx
@@ -6,22 +6,33 @@ import ItemForm from './ItemForm.jsx';
 class ListEntry extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      store: '',
+      total_price: 0.00,
+      items: this.props.list.items
+    }
+    console.log('2', this.props.stores)
   } // end constructor
 
+  handleStoreChange(e) {
+    this.setState({store: e.target.value});
+  }
   render() {
       return (
         <div>
           <h3>{this.props.list.name}</h3>
           <br/>
-         {/* <tr>
-            <th>Item</th>
-            <th>Qty</th>
-            <th>Price</th>
-          </tr>*/}
+          <select className="store-selection" onChange={this.handleStoreChange.bind(this)}>
+            <option key="new">New store</option>
+            {this.props.stores.map( (store) => {
+              return <option key={store._id} >{store}</option>
+            })}
+          </select>
+          <br/>
+          <br/>
           {
             // console.log('these are the items', this.state.items)
-            this.props.list.items.map((item) => {
-              console.log(item);
+            this.state.items.map((item) => {
               return <ListItemEntry key={item._id} item={item}/>
             })
           }
