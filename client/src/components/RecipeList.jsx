258682a2733f97b5447a31e22d5f06390ea0e907
yona <04l.yona@gmail.com>
2018-06-16 15:23:02 -0400

    render the health, diet, and ingredients label onto page

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -12,8 +12,13 @@ class RecipeList extends React.Component {
     console.log('from recipelist', this.props.hit.recipe)
     return (
       <div className="each-recipe">
-      <div><h3 className="recipe-title">{this.props.hit.recipe.label}</p></div>
-        <img src={this.props.hit.recipe.image}/>
+        <p className="recipe-title">{this.props.hit.recipe.label}</p>
+        <a href={this.props.hit.recipe.url}><img src={this.props.hit.recipe.image} target="_blank" className="recipe-pic"/></a>
+        <p className="recipe-diet">Diet: {this.props.hit.recipe.dietLabels.join(', ')}</p>
+        <p className="recipe-health">Health: {this.props.hit.recipe.healthLabels.join(', ')}</p>
+        <ul className="recipe-ingredients">
+          {this.props.hit.recipe.ingredientLines.map(eachIngredient => <li>{eachIngredient}</li>)}
+        </ul>
       </div>
     )
   }
