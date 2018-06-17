05cd79ec9f536dd4194232244388c24579c0a4f5
Kenny Le <kennyle37@gmail.com>
2018-06-16 17:08:22 -0700

    Remove timer from filter because it was giving user's issues

--- server/index.js
+++ server/index.js
@@ -68,10 +68,8 @@ app.get('/api/edamam/filter', (req, res) => {
   var id = process.env.RECIPELIST_API_ID || config.RECIPELIST_API_ID;
   var keys = process.env.RECIPELIST_API_KEYS || config.RECIPELIST_API_KEYS
 
-  request(`https://api.edamam.com/search?q=${req.query.q}&app_id=${id}&app_key=${keys}&from=0&to=3&calories=${req.query.calories}&time=${req.query.time}&diet=${req.query.diet}&health=${req.query.health}`, (error, response, body) => {
+  request(`https://api.edamam.com/search?q=${req.query.q}&app_id=${id}&app_key=${keys}&from=0&to=3&calories=${req.query.calories}&diet=${req.query.diet}&health=${req.query.health}`, (error, response, body) => {
     res.send(JSON.parse(body));
-    console.log(req.query)
-    console.log(req.query.health)
     console.log('sending back filtered recipe lists!');
   });
 });
